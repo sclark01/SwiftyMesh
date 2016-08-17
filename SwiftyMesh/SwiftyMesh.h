a0600c405acbb6a5c1490c2e36e87d2871591fee
Shea <sclark.tieche@gmail.com>
2016-08-17 14:14:03 -0700

    first commit

---
+++ SwiftyMesh/SwiftyMesh.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyMesh.h
+//  SwiftyMesh
+//
+//  Created by Shea Clark-Tieche on 8/17/16.
+//  Copyright © 2016 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyMesh.
+FOUNDATION_EXPORT double SwiftyMeshVersionNumber;
+
+//! Project version string for SwiftyMesh.
+FOUNDATION_EXPORT const unsigned char SwiftyMeshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyMesh/PublicHeader.h>
+
+
